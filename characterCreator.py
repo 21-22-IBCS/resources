5f7f04a9480d616af9cea0ae389b70c21aedbc49
Zack Considine <zack_considine@m015354.aw.org>
2022-02-15 13:53:45 -0800

    changed character Creator demo

--- characterCreator.py
+++ characterCreator.py
@@ -1,33 +1,48 @@
 from graphics import*
 from Button import*
 
+
 def main():
 
-    win = GraphWin("Character Creator", 800, 600)
+    win = GraphWin("Character Creator", 600, 600)
 
-    l = Line(Point(0,0),Point(400,300))
-    l.draw(win)
+    face = Oval(Point(200,100), Point(400,450))
+    face.draw(win)
 
-    c = Circle(Point(200,400),120)
-    c.draw(win)
+    nose = Polygon([Point(300, 200), Point(350, 280), Point(250, 280)])
+    #nose.draw(win)
+    nose2 = Circle(Point(300,250),20)
+    nose2.setFill('red')
 
-    o = Oval(Point(550,400),Point(650,450))
-    o.draw(win)
+    noseButton = Button(win, 'cyan', 'Large Nose', Point(200, 500), 70)
 
-    r = Rectangle(Point(550,400),Point(650,450))
-    r.draw(win)
-    r.setFill("red")
+    nose2Button = Button(win, 'yellow', 'Button Nose', Point(100, 500),60)
+    
+    quitButton = Button(win, 'red', "Quit", Point(500, 520), 60)
 
-    p = Polygon(Point(200,0),Point(250, 50), Point(150,120))
-    p.draw(win)
+    while True:
 
-    li = []
-    for i in range(5):
-        li.append(Point(300 + i*50, 200 - i*50))
-                  
-    p2 = Polygon(li)
-    p2.draw(win)
+        m1 = win.getMouse()
+        
+    
+        if quitButton.isClicked(m1):
+            break
 
+        if noseButton.isClicked(m1):
+            nose2.undraw()
+            nose.undraw()
+            nose.draw(win)
+
+        if nose2Button.isClicked(m1):
+            nose.undraw()
+            nose2.undraw()
+            nose2.draw(win)
+
+    win.close()
 
 if __name__ == "__main__":
     main()
+
+
+
+
